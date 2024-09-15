d84190774c82e34485a00ce8b13bfb0222e58656
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-15 14:10:35 +0300

    edte imgsa

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 let url = "https://rickandmortyapi.com/api/character/";
-        let characters = document.getElementById('container');
+let charactersContainer = document.getElementById('container');
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                characters.innerHTML = data.results.map(character => `
-                    <div class="character-card">
-                        <img src="${character.image}" alt="${character.name}">
-                        <p>${character.name}</p>
-                    </div>
-                `).join('');
-            });
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(character => {
+     
+            let characterCard = document.createElement('div');
+            characterCard.classList.add('character-card');
+
+            let nameParagraph = document.createElement('p');
+            nameParagraph.textContent = character.name;
+
+       
+            let imageElement = document.createElement('img');
+            imageElement.src = character.image;
+            imageElement.alt = character.name;
+
+            characterCard.appendChild(nameParagraph);
+            characterCard.appendChild(imageElement);
+
+            charactersContainer.appendChild(characterCard);
+        });
+    });
